5eab8c6538a44bcc5468531eb7e2912f072274e5
zeNn-G <treroin@gmail.com>
2022-09-28 19:04:48 +0300

    Boiler Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
+import React from "react";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import Register from "./screens/Register";
+import NotFound from "./screens/NotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
 function App() {
-  return <div className="">Test</div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
