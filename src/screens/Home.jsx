8e79a1abefbd450c7fd389221711a4d364a5b76b
Erkan Ozcanoglu <ozcanogerkan12@gmail.com>
2022-10-03 14:50:13 +0300

    Navbar

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import supabase from "../config/supaBaseClient";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
   console.log(supabase);
-  return <div>Home</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default Home;
