b267505d741a07fc139b9e28b7f17c4c8833de76
zeNn-G <treroin@gmail.com>
2022-10-03 15:22:08 +0300

    Input

--- src/screens/Register.jsx
+++ src/screens/Register.jsx
@@ -35,7 +35,7 @@ const Register = () => {
                   type="email"
                   name="email"
                   id="email"
-                  className="border focus:border-white ring-1 focus:ring-2 focus:ring-primary ring-red-200 border-white text-white sm:text-sm rounded-lg  block w-full p-2.5 bg-lightSecond  placeholder-gray-400 "
+                  className="input-form"
                   placeholder="name@company.com"
                   required=""
                 />
@@ -52,7 +52,7 @@ const Register = () => {
                   name="password"
                   id="password"
                   placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="input-form"
                   required=""
                 />
               </div>
@@ -68,7 +68,7 @@ const Register = () => {
                   name="confirm-password"
                   id="confirm-password"
                   placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="input-form"
                   required=""
                 />
               </div>
