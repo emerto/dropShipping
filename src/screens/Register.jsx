835414a7ac8c358b549e964ef547530ad86b97cf
zeNn-G <treroin@gmail.com>
2022-10-03 15:36:48 +0300

    Register

--- src/screens/Register.jsx
+++ src/screens/Register.jsx
@@ -20,7 +20,7 @@ const Register = () => {
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Create{" "}
               <span className="text-black bg-primary p-2 rounded-3xl">
-                and account
+                an account
               </span>
             </h1>
             <form className="space-y-4 md:space-y-6" action="#">
@@ -99,7 +99,7 @@ const Register = () => {
               </div>
               <button
                 type="submit"
-                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                className="w-full text-black bg-accent hover:bg-primary/90 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
               >
                 Create an account
               </button>
