aa9e0b0cfb66207c72ef6f9124cd8cf0c0b2536e
Tolga Uğur <29550889+Tolgaugur@users.noreply.github.com>
2022-10-04 21:52:12 +0300

    navbar comment

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import supabase from "../config/supaBaseClient";
 import Navbar from "../components/Navbar";
-
+import StoreForm from "../components/StoreForm";
 const Home = () => {
   console.log(supabase);
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
+      <StoreForm />
     </div>
   );
 };
--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -7,7 +7,7 @@ const Login = () => {
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <Link
           to="/"
-          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+          className="flex items-center mb-6 text-2xl font-semibold text-gray-900"
         >
           <img
             className="w-[400px] h-[100px] object-cover "
