e32a6940e08abc5f45a0b1182640e0c9bf3acefe
Erkan Ozcanoglu <ozcanogerkan12@gmail.com>
2022-10-06 20:14:10 +0300

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -86,147 +86,127 @@ const Navbar = () => {
                 className="bg-transparent outline-none w-[200px]  hamburger:w-[400px] text-black placeholder-gray-600 "
               />
             </div>
-            <li
-              className="flex md:mt-2 mt-10 "
-              onMouseEnter={() => {
-                lottie.setDirection(1);
-                lottie.play("home");
-              }}
-              onMouseLeave={() => {
-                lottie.setDirection(-1);
-                lottie.play("home");
-              }}
+            <div
+              className={`hamburger:flex  text-orange-400 mr-0 hamburger:items-center hamburger:pb-0 pb-12 absolute hamburger:static 
+           hamburger:z-auto z-[-1] left-0 hamburger:pl-0 pl-0 transition-all 
+          duration-500 ease-linear `}
             >
-              {/* <NavLink
-                className="italic hamburger:not-italic text-lg flex items-center gap-x-4
-                cursor-pointer "
-                to="/"
-                style={{ color: "inherit", backgroundColor: "inherit" }}
+              <li
+                className="flex hamburger:mr-16 hamburger:mt-2 mt-2"
+                onMouseEnter={() => {
+                  lottie.setDirection(1);
+                  lottie.play("info");
+                }}
+                onMouseLeave={() => {
+                  lottie.stop("info");
+                }}
               >
-                <div
-                  className={`flex hamburger:ml-1 ml-0 hamburger:block  
-                  duration-500`}
+                <Link
+                  className="italic hamburger:not-italic text-lg flex items-center gap-x-4
+                  cursor-pointer "
+                  to="about"
+                  smooth={true}
+                  style={{ color: "inherit", backgroundColor: "inherit" }}
                 >
-                  <div ref={homeRef} className="w-[35px] h-[35px]" />
-                </div>
-                Anasayfa
-              </NavLink> */}
-            </li>
-            <li
-              className="flex hamburger:mr-16 hamburger:mt-2 mt-2"
-              onMouseEnter={() => {
-                lottie.setDirection(1);
-                lottie.play("info");
-              }}
-              onMouseLeave={() => {
-                lottie.stop("info");
-              }}
-            >
-              <Link
-                className="italic hamburger:not-italic text-lg flex items-center gap-x-4
-                cursor-pointer "
-                to="about"
-                smooth={true}
-                style={{ color: "inherit", backgroundColor: "inherit" }}
-              >
-                <div
-                  className={`flex hamburger:ml-1 ml-0 hamburger:block 
-                  duration-500`}
-                >
-                  <div ref={infoRef} />
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
+                  <div
+                    className={`flex hamburger:ml-1 ml-0 hamburger:block
+                    duration-500`}
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-                    />
-                  </svg>
-                </div>
-              </Link>
-            </li>
-            <li
-              className="flex hamburger:mr-16 hamburger:mt-2 mt-10"
-              onMouseEnter={() => {
-                lottie.play("services");
-              }}
-              onMouseLeave={() => {
-                lottie.stop("services");
-              }}
-            >
-              <NavLink
-                className="italic hamburger:not-italic text-lg flex items-center gap-x-4
-                cursor-pointer "
-                to="/StoreForm"
-                style={{ color: "inherit", backgroundColor: "inherit" }}
+                    <div ref={infoRef} />
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                      />
+                    </svg>
+                  </div>
+                </Link>
+              </li>
+              <li
+                className="flex hamburger:mr-16 hamburger:mt-2 mt-10"
+                onMouseEnter={() => {
+                  lottie.play("services");
+                }}
+                onMouseLeave={() => {
+                  lottie.stop("services");
+                }}
               >
-                <div
-                  className={`flex hamburger:ml-0 ml-0 hamburger:block 
-                  duration-500`}
+                <NavLink
+                  className="italic hamburger:not-italic text-lg flex items-center gap-x-4
+                  cursor-pointer "
+                  to="/StoreForm"
+                  style={{ color: "inherit", backgroundColor: "inherit" }}
                 >
-                  <div ref={serviceRef} />
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
+                  <div
+                    className={`flex hamburger:ml-0 ml-0 hamburger:block
+                    duration-500`}
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z"
-                    />
-                  </svg>
-                </div>
-              </NavLink>
-            </li>
-            <li
-              className="flex hamburger:mr-[100px] hamburger:mt-2 mt-10"
-              onMouseEnter={() => {
-                lottie.setDirection(1);
-                lottie.play("contact");
-              }}
-              onMouseLeave={() => {
-                lottie.stop("contact");
-              }}
-            >
-              <Link
-                className="italic hamburger:not-italic text-lg flex items-center gap-x-4
-                cursor-pointer "
-                to="contact"
-                smooth={true}
-                style={{ color: "inherit", backgroundColor: "inherit" }}
+                    <div ref={serviceRef} />
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z"
+                      />
+                    </svg>
+                  </div>
+                </NavLink>
+              </li>
+              <li
+                className="flex hamburger:mr-[100px] hamburger:mt-2 mt-10"
+                onMouseEnter={() => {
+                  lottie.setDirection(1);
+                  lottie.play("contact");
+                }}
+                onMouseLeave={() => {
+                  lottie.stop("contact");
+                }}
               >
-                <div
-                  className={`flex ml-0 hamburger:ml-0 hamburger:block 
-                  duration-500`}
+                <Link
+                  className="italic hamburger:not-italic text-lg flex items-center gap-x-4
+                  cursor-pointer "
+                  to="contact"
+                  smooth={true}
+                  style={{ color: "inherit", backgroundColor: "inherit" }}
                 >
-                  <div ref={contactRef} />
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
+                  <div
+                    className={`flex ml-0 hamburger:ml-0 hamburger:block
+                    duration-500`}
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-                    />
-                  </svg>
-                </div>
-              </Link>
-            </li>
+                    <div ref={contactRef} />
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                      />
+                    </svg>
+                  </div>
+                </Link>
+              </li>
+            </div>
           </ul>
         </div>
       </div>
