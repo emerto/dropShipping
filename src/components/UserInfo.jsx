6a7bb3d4417acc7bfe47956154f1fb064b2be977
Erkan Ozcanoglu <ozcanogerkan12@gmail.com>
2022-10-06 21:39:15 +0300

    Sorry Erol

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -1,46 +1,93 @@
 import React from "react";
 
-const UserInfo = () => {
+const Profile = () => {
   return (
-    <div>
-      <div className="flex flex-col gap-4">
-        <div>
-          <div className="mb-2 block">
-            <Label htmlFor="username3" color="green" value="Your name" />
+    <div className="flex justify-center">
+      <div className="mt-[100px] w-[85vw] flex justify-center  bg-slate-500">
+        <form>
+          <div className="flex flex-col">
+            <div className="flex flex-row">
+              <div className="relative z-0 justify-center flex mb-6 mr-5 group">
+                <input
+                  type="email"
+                  name="floating_email"
+                  id="floating_email"
+                  className="block py-2.5 px-0 w-[350px] text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
+                  required
+                />
+                <label
+                  for="floating_email"
+                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >
+                  First Name
+                </label>
+              </div>
+              <div className="relative z-0 justify-center flex mb-6 ml-5 group">
+                <input
+                  type="email"
+                  name="floating_email"
+                  id="floating_email"
+                  className="block py-2.5 px-0 w-[350px] text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
+                  required
+                />
+                <label
+                  for="floating_email"
+                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >
+                  Last Name
+                </label>
+              </div>
+            </div>
+            <div className="flex flex-row">
+              <div className="relative z-0 justify-center flex mb-6 mr-5">
+                <input
+                  type="email"
+                  name="floating_email"
+                  id="floating_email"
+                  className="block py-2.5 px-0 w-[350px] text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
+                  required
+                />
+                <label
+                  for="floating_email"
+                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >
+                  User Name
+                </label>
+              </div>
+              <div className="relative z-0 justify-center flex mb-6 ml-5 ">
+                <input
+                  type="email"
+                  name="floating_email"
+                  id="floating_email"
+                  className="block py-2.5 px-0 w-[350px] text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
+                  required
+                />
+                <label
+                  for="floating_email"
+                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >
+                  Address
+                </label>
+              </div>
+            </div>
           </div>
-          <TextInput
-            id="username"
-            placeholder="Bonnie Green"
-            required={true}
-            color="green"
-            helperText={
-              <React.Fragment>
-                <span className="font-medium">Alright!</span> Username
-                available!
-              </React.Fragment>
-            }
-          />
-        </div>
-        <div>
-          <div className="mb-2 block">
-            <Label htmlFor="username4" color="red" value="Your name" />
+
+          <div className="flex justify-start">
+            <button
+              type="submit"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Submit
+            </button>
           </div>
-          <TextInput
-            id="username4"
-            placeholder="Bonnie Green"
-            required={true}
-            color="red"
-            helperText={
-              <React.Fragment>
-                <span className="font-medium">Oops!</span> Username already
-                taken!
-              </React.Fragment>
-            }
-          />
-        </div>
+        </form>
       </div>
     </div>
   );
 };
 
-export default UserInfo;
+export default Profile;
--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -1,93 +1,38 @@
 import React from "react";
+import supabase from "../config/supaBaseClient";
 
-const Profile = () => {
+import UserInfo from "../components/UserInfo";
+
+const FirstAcc = () => {
+  const updateProfile = async (e) => {
+    try {
+      const user = supabase.auth.user();
+
+      const updates = {
+        id: user.id,
+        username: "kazik",
+        website: "kazik.com",
+        avatar_url: "https://kazik.com",
+        updated_at: new Date(),
+      };
+
+      let { error } = await supabase
+        .from("profiles")
+        .upsert(updates, { returning: "minimal" });
+
+      if (error) {
+        throw error;
+      }
+    } catch (err) {
+      alert(err);
+    }
+  };
+
   return (
-    <div className="flex justify-center">
-      <div className="mt-[100px] w-[85vw] flex justify-center  bg-slate-500">
-        <form>
-          <div className="flex flex-col">
-            <div className="flex flex-row">
-              <div className="relative z-0 justify-center flex mb-6 mr-5 group">
-                <input
-                  type="email"
-                  name="floating_email"
-                  id="floating_email"
-                  className="block py-2.5 px-0 w-[350px] text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                  placeholder=" "
-                  required
-                />
-                <label
-                  for="floating_email"
-                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >
-                  First Name
-                </label>
-              </div>
-              <div className="relative z-0 justify-center flex mb-6 ml-5 group">
-                <input
-                  type="email"
-                  name="floating_email"
-                  id="floating_email"
-                  className="block py-2.5 px-0 w-[350px] text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                  placeholder=" "
-                  required
-                />
-                <label
-                  for="floating_email"
-                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >
-                  Last Name
-                </label>
-              </div>
-            </div>
-            <div className="flex flex-row">
-              <div className="relative z-0 justify-center flex mb-6 mr-5">
-                <input
-                  type="email"
-                  name="floating_email"
-                  id="floating_email"
-                  className="block py-2.5 px-0 w-[350px] text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                  placeholder=" "
-                  required
-                />
-                <label
-                  for="floating_email"
-                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >
-                  User Name
-                </label>
-              </div>
-              <div className="relative z-0 justify-center flex mb-6 ml-5 ">
-                <input
-                  type="email"
-                  name="floating_email"
-                  id="floating_email"
-                  className="block py-2.5 px-0 w-[350px] text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                  placeholder=" "
-                  required
-                />
-                <label
-                  for="floating_email"
-                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >
-                  Address
-                </label>
-              </div>
-            </div>
-          </div>
-
-          <div className="flex justify-start">
-            <button
-              type="submit"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Submit
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+    <button onClick={updateProfile}>
+      <UserInfo />
+    </button>
   );
 };
 
-export default Profile;
+export default FirstAcc;
