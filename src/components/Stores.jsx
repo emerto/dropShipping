53d4eda96f7f10f89ed7ac0d6e71f4f4c925e44d
Tolga Uğur <29550889+Tolgaugur@users.noreply.github.com>
2022-10-06 21:58:52 +0300

    Store

--- src/components/Stores.jsx
+++ src/components/Stores.jsx
@@ -1,22 +1,33 @@
 import React from "react";
 import kazik from "../assets/kazik.png";
+import { ShoppingCartIcon } from "@heroicons/react/24/outline";
+
 const Stores = () => {
   return (
-    <section className="bg-gray-900 mt-3">
-      <div className="flex flex-col  ">
-        <div className="border-2 flex flex-col gap-16 items-center py-8 px-4 mx-auto  ">
-          <div className="gap-4 mt-8 flex center">
+    <section className="bg-gray-900 flex justify-center">
+      <div className="flex flex-row mt-[150px] w-[250px]">
+        <div className="border-2 flex flex-col gap-5 py-3 px-4 mx-auto w-[800px]  ">
+          <div className="gap-4 mt-8 flex justify-center ">
             <img
-              className="max-w-[350px] max-h-[350px] rounded-lg align-middle flex center"
+              className="w-max max-h-[200px] rounded-lg "
               src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png"
               alt="office content 1"
             />
           </div>
-          <div className="font-light text-gray-500 sm:text-lg ">
-            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-white ">
-              We didn't reinvent the wheel
+          <div className=" sm:text-lg  ">
+            <h2 className="mb-2 flex text-xl tracking-tight font-normal text-white  ">
+              Erol's Kayganlastirici
             </h2>
           </div>
+
+          <div className="flex justify-end">
+            <div className="border-1 flex  bg-primary rounded-2xl w-[50px] h-max justify-center ">
+              <p className="text-black  text-sm font-bold ">12$</p>
+            </div>
+          </div>
+          <button className=" bg-gray-800  rounded-2xl flex justify-center py-3 h-fill w-fill hover:bg-gray-700">
+            <ShoppingCartIcon className="h-[25px] end w-[50px]  text-primary" />
+          </button>
         </div>
       </div>
     </section>
