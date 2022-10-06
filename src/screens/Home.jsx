ad9f982ea7796294d9488c491c80cf239d6ab2a9
Erkan Ozcanoglu <ozcanogerkan12@gmail.com>
2022-10-06 20:20:13 +0300

    .

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import supabase from "../config/supaBaseClient";
 import Navbar from "../components/Navbar";
 import StoreForm from "../components/StoreForm";
-// import Slider from "../components/Slider";
+
 const Home = () => {
-  console.log(supabase);
   return (
-    <div className="bg-gray-900">
+    <div className=" bg-gray-900 h-[100vh]">
       <Navbar />
     </div>
   );
