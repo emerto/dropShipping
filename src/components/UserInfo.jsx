c84e1e8c18c959fb553b79bc78557a266eeb93a5
Erkan Ozcanoglu <ozcanogerkan12@gmail.com>
2022-10-06 21:45:51 +0300

    .

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -7,51 +7,51 @@ const Profile = () => {
         <form>
           <div className="flex flex-col">
             <div className="flex flex-row">
-              <div className="relative z-0 justify-center flex mb-6 mr-5 group">
+              <div className="relative z-0 justify-center flex mb-6 ml-5 group">
                 <input
-                  type="email"
-                  name="floating_email"
-                  id="floating_email"
+                  type="first_name"
+                  name="floating_first_name"
+                  id="floating_first_name"
                   className="block py-2.5 px-0 w-[350px] text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" "
                   required
                 />
                 <label
-                  for="floating_email"
+                  for="floating_first_name"
                   className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
                   First Name
-                </label>
-              </div>
-              <div className="relative z-0 justify-center flex mb-6 ml-5 group">
-                <input
-                  type="email"
-                  name="floating_email"
-                  id="floating_email"
-                  className="block py-2.5 px-0 w-[350px] text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                  placeholder=" "
-                  required
-                />
-                <label
-                  for="floating_email"
-                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >
-                  Last Name
                 </label>
               </div>
             </div>
             <div className="flex flex-row">
               <div className="relative z-0 justify-center flex mb-6 mr-5">
                 <input
-                  type="email"
-                  name="floating_email"
-                  id="floating_email"
+                  type="last_name"
+                  name="floating_last_name"
+                  id="floating_last_name"
                   className="block py-2.5 px-0 w-[350px] text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" "
                   required
                 />
                 <label
-                  for="floating_email"
+                  for="floating_last_name"
+                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >
+                  Last Name
+                </label>
+              </div>
+              <div className="relative z-0 justify-center flex mb-6 ml-5 ">
+                <input
+                  type="user_name"
+                  name="floating_user_name"
+                  id="floating_user_name"
+                  className="block py-2.5 px-0 w-[350px] text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
+                  required
+                />
+                <label
+                  for="floating_user_name"
                   className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
                   User Name
@@ -59,15 +59,15 @@ const Profile = () => {
               </div>
               <div className="relative z-0 justify-center flex mb-6 ml-5 ">
                 <input
-                  type="email"
-                  name="floating_email"
-                  id="floating_email"
+                  type="address"
+                  name="floating_address"
+                  id="floating_address"
                   className="block py-2.5 px-0 w-[350px] text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" "
                   required
                 />
                 <label
-                  for="floating_email"
+                  for="floating_address"
                   className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
                   Address
