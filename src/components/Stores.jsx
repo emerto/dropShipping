ba5ea52e29ec56c3bc7fba4044518d951cfab1c8
Tolga Uğur <29550889+Tolgaugur@users.noreply.github.com>
2022-10-06 20:50:21 +0300

    stores

--- src/components/Stores.jsx
+++ src/components/Stores.jsx
@@ -1,7 +1,26 @@
 import React from "react";
-
+import kazik from "../assets/kazik.png";
 const Stores = () => {
-  return <div></div>;
+  return (
+    <section className="bg-gray-900 mt-3">
+      <div className="flex flex-col  ">
+        <div className="border-2 flex flex-col gap-16 items-center py-8 px-4 mx-auto  ">
+          <div className="gap-4 mt-8 flex center">
+            <img
+              className="max-w-[350px] max-h-[350px] rounded-lg align-middle flex center"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png"
+              alt="office content 1"
+            />
+          </div>
+          <div className="font-light text-gray-500 sm:text-lg ">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-white ">
+              We didn't reinvent the wheel
+            </h2>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Stores;
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import StoreForm from "../components/StoreForm";
+import Stores from "../components/Stores";
 
 const Home = () => {
   return (
     <div className=" bg-gray-900 h-[100vh]">
       <Navbar />
+      <Stores />
     </div>
   );
 };
