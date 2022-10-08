0235bd0c8ba4e6705a623feed65c7d9ad8d4991e
zeNn-G <treroin@gmail.com>
2022-10-08 14:11:58 +0300

    Debug

---
+++ src/components/UserInfo.jsx
@@ -0,0 +1,132 @@
+import React, { useState } from "react";
+import supabase from "../config/supaBaseClient";
+
+const Profile = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [address, setAddress] = useState("");
+
+  const updateProfile = async (e) => {
+    e.preventDefault();
+
+    try {
+      const user = supabase.auth.user();
+
+      const updates = {
+        id: user.id,
+        email: user.email,
+        first_name: firstName,
+        last_name: lastName,
+        username: username,
+        address: address,
+        avatar_url: "https://kazik.com",
+        updated_at: new Date(),
+      };
+
+      let { error } = await supabase
+        .from("profiles")
+        .upsert(updates, { returning: "minimal" });
+
+      if (error) {
+        throw error;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="flex justify-center">
+      <div className="mt-[100px] w-[85vw] flex justify-center  bg-slate-500">
+        <form onSubmit={updateProfile}>
+          <div className="flex flex-col">
+            <div className="flex flex-row">
+              <div className="relative z-0 justify-center flex mb-6 ml-5 group">
+                <input
+                  type="first_name"
+                  name="floating_first_name"
+                  id="floating_first_name"
+                  className="block py-2.5 px-0 w-[350px] text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
+                  required
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+                <label
+                  for="floating_first_name"
+                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >
+                  First Name
+                </label>
+              </div>
+            </div>
+            <div className="flex flex-row">
+              <div className="relative z-0 justify-center flex mb-6 mr-5">
+                <input
+                  type="last_name"
+                  name="floating_last_name"
+                  id="floating_last_name"
+                  className="block py-2.5 px-0 w-[350px] text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
+                  required
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+                <label
+                  for="floating_last_name"
+                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >
+                  Last Name
+                </label>
+              </div>
+              <div className="relative z-0 justify-center flex mb-6 ml-5 ">
+                <input
+                  type="user_name"
+                  name="floating_user_name"
+                  id="floating_user_name"
+                  className="block py-2.5 px-0 w-[350px] text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
+                  required
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+                <label
+                  for="floating_user_name"
+                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >
+                  User Name
+                </label>
+              </div>
+              <div className="relative z-0 justify-center flex mb-6 ml-5 ">
+                <input
+                  type="address"
+                  name="floating_address"
+                  id="floating_address"
+                  className="block py-2.5 px-0 w-[350px] text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
+                  required
+                  onChange={(e) => setAddress(e.target.value)}
+                />
+                <label
+                  for="floating_address"
+                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >
+                  Address
+                </label>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex justify-start">
+            <button
+              type="submit"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
