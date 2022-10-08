76e80d9b1ec03f88d9aa80c6049a51cc986f6c4e
Tolga Uğur <29550889+Tolgaugur@users.noreply.github.com>
2022-10-08 20:25:21 +0300

    StoreWithProduct

---
+++ src/components/StoreWithProducts.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Stores from "./Stores";
+import kazik from "../assets/kazik.png";
+const StoreWithProducts = () => {
+  return <div></div>;
+};
+
+export default StoreWithProducts;
