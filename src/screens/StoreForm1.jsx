70a5a63003c245e892693d0be9f4db20bef76e22
zeNn-G <treroin@gmail.com>
2022-10-09 00:02:49 +0300

    The Rock

--- src/screens/StoreForm1.jsx
+++ src/screens/StoreForm1.jsx
@@ -3,13 +3,31 @@ import StoreForm from "../components/StoreForm";
 import Navbar from "../components/Navbar";
 
 import { useAuth } from "../context/AuthContext";
+import { Link } from "react-router-dom";
 
 const StoreForm1 = () => {
   const auth = useAuth();
   return (
     <div>
       <Navbar />
-      {auth.user ? <StoreForm></StoreForm> : <h1>you are not logged in</h1>}
+      {auth.user ? (
+        <StoreForm />
+      ) : (
+        <div className="w-[100vw] h-[100vh] bg-gray-900 flex justify-center items-center">
+          <div className="flex flex-col w-[1000px] justify-center items-center gap-5">
+            <img
+              src="https://c.tenor.com/C9ptDG1X42YAAAAC/rock-eyebrow.gif"
+              className="w-[400px] h-[400px] rounded-lg"
+            />
+            <p className="text-white">
+              Seems like you are not logged in.
+              <span className="font-medium text-primary-600 hover:underline text-primary cursor-pointer">
+                <Link to="/login"> Please login to continue!</Link>
+              </span>
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
