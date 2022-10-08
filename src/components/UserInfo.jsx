a19aff58a73d3e99b9c60636e2e52abe8cd993eb
zeNn-G <treroin@gmail.com>
2022-10-08 22:27:47 +0300

    User

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -4,18 +4,55 @@ import supabase from "../config/supaBaseClient";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const Profile = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [username, setUsername] = useState("");
   const [address, setAddress] = useState("");
+  const [email, setEmail] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
 
   const auth = useAuth();
   const navigate = useNavigate();
 
+  const getProfile = async () => {
+    const { data, error } = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("id", auth.user.id)
+      .single();
+
+    setFirstName(data.first_name);
+    setLastName(data.last_name);
+    setUsername(data.username);
+    setAddress(data.address);
+    setPhoneNumber(data.phone_number);
+    setEmail(data.email);
+  };
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
   const updateProfile = async (e) => {
     e.preventDefault();
+
+    if (!firstName || !lastName || !username || !address || !phoneNumber) {
+      toast.error(`Please fill all fields correctly!`, {
+        position: "bottom-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+      return;
+    }
 
     try {
       const user = supabase.auth.user();
@@ -52,6 +89,17 @@ const Profile = () => {
 
   return (
     <section>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <div className=" ">
         <div className="flex flex-col items-end w-[80vw] mt-0 px-6 py-8 mx-auto lg:py-0">
           <div className="w-full rounded-lg shadow dark:border md:mt-0 max-w-[50vw] xl:p-0 bg-secondary border-primary">
@@ -76,9 +124,8 @@ const Profile = () => {
                       type="first_name"
                       name="floating_first_name"
                       id="floating_first_name"
-                      placeholder="First Name"
+                      placeholder={firstName}
                       className="input-form bg-secondary focus:bg-neutral-700"
-                      required
                       onChange={(e) => setFirstName(e.target.value)}
                     />
                   </div>
@@ -93,9 +140,8 @@ const Profile = () => {
                       type="last_name"
                       name="floating_last_name"
                       id="floating_last_name"
-                      placeholder="Last Name"
+                      placeholder={lastName}
                       className="input-form bg-secondary focus:bg-neutral-700"
-                      required
                       onChange={(e) => setLastName(e.target.value)}
                     />
                   </div>
@@ -113,9 +159,8 @@ const Profile = () => {
                       type="username"
                       name="floating_username"
                       id="floating_username"
-                      placeholder="Username"
+                      placeholder={username}
                       className="input-form bg-secondary focus:bg-neutral-700"
-                      required
                       onChange={(e) => setUsername(e.target.value)}
                     />
                   </div>
@@ -130,9 +175,8 @@ const Profile = () => {
                       type="telephone"
                       name="floating_telephone"
                       id="floating_telephone"
-                      placeholder="Telephone"
+                      placeholder={phoneNumber}
                       className="input-form bg-secondary focus:bg-neutral-700"
-                      required
                       onChange={(e) => setPhoneNumber(e.target.value)}
                     />
                   </div>
@@ -149,23 +193,7 @@ const Profile = () => {
                     type="email"
                     name="floating_email"
                     id="floating_email"
-                    placeholder="Email"
-                    className="input-form bg-secondary focus:bg-neutral-700"
-                    disabled
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="password"
-                    className="block mb-2 text-start text-sm font-medium text-gray-300"
-                  >
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    name="floating_password"
-                    id="floating_password"
-                    placeholder="********"
+                    placeholder={email}
                     className="input-form bg-secondary focus:bg-neutral-700"
                     disabled
                   />
@@ -181,9 +209,8 @@ const Profile = () => {
                     type="address"
                     name="floating_address"
                     id="floating_address"
-                    placeholder="Address"
+                    placeholder={address}
                     className="input-form p-7 bg-secondary focus:bg-neutral-700"
-                    required
                     onChange={(e) => setAddress(e.target.value)}
                   />
                 </div>
