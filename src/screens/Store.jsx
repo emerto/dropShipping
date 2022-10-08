6693ca9fe3223f2686aba45d8602585c0ff90d78
ArdaGunonu <arda.gunonu@hotmail.com>
2022-10-08 22:10:21 +0300

    Store

--- src/screens/Store.jsx
+++ src/screens/Store.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import ProductCard from "../components/ProductCard";
+import Navbar from "../components/Navbar";
 
 const Store = () => {
   return (
-    <div>
-      <div></div>
+    <div className="bg-black mt-24">
+      <div>
+        <Navbar />
+      </div>
+      <div className="text-primary ml-8 text-4xl font-semibold">
+        <div></div>
+        <h1>Store Name</h1>
+      </div>
+      <div className="grid grid-cols-4 bg-gray-900">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
     </div>
   );
 };
