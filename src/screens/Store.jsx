e4552183562aed9e0148f779a47de30d252ac362
Erkan Ozcanoglu <ozcanogerkan12@gmail.com>
2022-10-16 00:47:45 +0300

    .

--- src/screens/Store.jsx
+++ src/screens/Store.jsx
@@ -15,7 +15,6 @@ const Store = () => {
           <h1 className=" ml-8 pt-5 mt-5 border-b-2 w-[250px] flex justify-center">
             Store
           </h1>
-          <ModalPopup />
         </div>
         <div className="mx-36 grid xl:grid-cols-3 grid-cols-2 bg-gray-900">
           <div className="ml-4 mr-4 mt-10 border-2 border-primary rounded-lg ">
