c48ccdefad3321787b69b1d187b73f7e740054a3
zeNn-G <treroin@gmail.com>
2022-10-16 01:09:34 +0300

    State

--- src/screens/ManageStore.jsx
+++ src/screens/ManageStore.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Navbar from "../components/Navbar";
+
+import supabase from "../config/supaBaseClient";
 import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const ManageStore = () => {
-  const { state } = useLocation();
-  console.log(state);
+  const location = useLocation();
+  const storeInfo = location.state;
+
+  const { owner, store_name, store_description, store_image } = storeInfo;
+
+  const user = supabase.auth.user();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/");
+    }
+
+    if (user.id != owner) {
+      navigate("/");
+    }
+  }, []);
+
   return (
     <>
       <Navbar />
