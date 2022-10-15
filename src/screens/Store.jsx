9daac29f8737b53b4f58afadc18566910bfe29f9
Erkan Ozcanoglu <ozcanogerkan12@gmail.com>
2022-10-16 00:20:39 +0300

    .

--- src/screens/Store.jsx
+++ src/screens/Store.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ProductCard from "../components/ProductCard";
 import Navbar from "../components/Navbar";
 import { useLocation } from "react-router-dom";
-// import ModalPopup from "../components/ModalPopup";
 
 const Store = () => {
   // const { state } = useLocation();
