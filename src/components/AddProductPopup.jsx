b5cee3cd5562b6a789760697f06da01e0b6c00e3
ArdaGunonu <arda.gunonu@hotmail.com>
2022-10-16 12:50:16 +0300

    pop

--- src/components/AddProductPopup.jsx
+++ src/components/AddProductPopup.jsx
@@ -9,6 +9,13 @@ import {
 } from "semantic-ui-react";
 
 const AddProductPopup = () => {
+  const products = [
+    { key: 1, text: "Product 1", value: 1 },
+    { key: 2, text: "Product 2", value: 2 },
+    { key: 3, text: "Product 3", value: 3 },
+    { key: 4, text: "Product 4", value: 4 },
+    { key: 5, text: "Product 5", value: 5 },
+  ];
   const [Show, setShow] = useState(false);
   return (
     <Modal
@@ -23,7 +30,50 @@ const AddProductPopup = () => {
     >
       <Modal.Header>Add product</Modal.Header>
       <Modal.Content>
-        <h1>dasdasd</h1>
+        <form className="space-y-4 md:space-y-6" action="Submit">
+          <div>
+            <label
+              htmlFor="productName"
+              className="block mb-2 text-start w-auto text-sm font-medium text-gray-800"
+            >
+              Product name
+            </label>
+            <input
+              type="productName"
+              name="fProductName"
+              id="fProductName"
+              placeholder="Enter product name"
+              className="input-form bg-secondary focus:bg-neutral-700"
+            />
+            <label
+              htmlFor="price"
+              className="block mb-2 text-start w-auto text-sm font-medium text-gray-800"
+            >
+              Price
+            </label>
+            <input
+              type="price"
+              name="fPrice"
+              id="fPrice"
+              placeholder="Enter price"
+              className="input-form bg-secondary focus:bg-neutral-700"
+            />
+            <label
+              htmlFor="product"
+              className="block mb-2 text-start w-auto text-sm font-medium text-gray-800"
+            >
+              Product
+            </label>
+            <Dropdown
+              placeholder="Select product"
+              fluid
+              search
+              selection
+              options={products}
+              style={{ backgroundColor: "#1A1A1A", color: "white" }}
+            />
+          </div>
+        </form>
       </Modal.Content>
       <Modal.Actions>
         <button onClick={() => setShow(false)}>Close</button>
