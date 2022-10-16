eb8e474d61a65b49a0c5d18ff3ea479c905c407d
Tolga Uğur <29550889+Tolgaugur@users.noreply.github.com>
2022-10-16 14:55:52 +0300

    a

--- src/components/EditProductPop.jsx
+++ src/components/EditProductPop.jsx
@@ -1,59 +1,76 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
+import {
+  Button,
+  Header,
+  Image,
+  Modal,
+  Input,
+  Dropdown,
+} from "semantic-ui-react";
 
-const EditProductPop = () => {
-  const [showModal, setShowModal] = useState(false);
-
-  function openModal() {
-    setShowModal(true);
-  }
-  function closeModal() {
-    setShowModal(false);
-  }
-
+const AddProductPopup = () => {
+  const [Show, setShow] = useState(false);
   return (
-    <div className="bg-red-600">
-      <button onClick={openModal}>Click Me</button>
-      <div
-        isOpen={showModal}
-        contentLabel="This is a pop up"
-        className="mx-20 mt-40 bg-gray-900 border border-white"
-      >
-        <div className="m-5"></div>
-        <form>
-          <label className="block mb-2 text-start w-auto text-sm font-medium text-gray-300">
-            Name
-          </label>
-          <input />
-          <label className="block mb-2 text-start w-auto text-sm font-medium text-gray-300">
-            Price
-          </label>
-          <input />
-          <label className="block mb-2 text-start w-auto text-sm font-medium text-gray-300">
-            Product
-          </label>
-          <div className="w-full">
+    <Modal
+      onClose={() => setShow(false)}
+      onOpen={() => setShow(true)}
+      open={Show}
+      trigger={
+        <button className="bg-primary rounded-2xl text-black">
+          Show Modal
+        </button>
+      }
+    >
+      <Modal.Header>
+        <div>
+          Edit Product
+          <Modal.Actions>
+            <div className="flex justify-end">
+              <div className="flex border py-2   rounded-xl bg-primary">
+                <button className="flex " onClick={() => setShow(false)}>
+                  Close
+                </button>
+              </div>
+            </div>
+          </Modal.Actions>
+        </div>
+      </Modal.Header>
+
+      <Modal.Content>
+        <form className="space-y-4 md:space-y-6" action="Submit">
+          <div>
             <label
-              className="block mb-2 text-start text-sm font-medium text-gray-300"
-              for="user_avatar"
+              htmlFor="productName"
+              className="block mb-2 text-start w-auto text-sm font-medium text-gray-800"
             >
-              Upload an image
+              Product name
             </label>
             <input
-              className="block  w-full text-sm  rounded-lg border  cursor-pointer text-gray-400 focus:outline-none bg-secondary border-primary placeholder-gray-400"
-              aria-describedby="user_avatar_help"
-              id="user_avatar"
-              type="file"
-              accept={"image/jpeg image/png"}
+              type="productName"
+              name="fProductName"
+              id="fProductName"
+              placeholder="Enter product name"
+              className="input-form w-auto bg-secondary focus:bg-neutral-700"
+            />
+            <label
+              htmlFor="price"
+              className="block mb-2 text-start w-auto text-sm font-medium text-gray-800"
+            >
+              Price
+            </label>
+            <input
+              type="price"
+              name="fPrice"
+              id="fPrice"
+              placeholder="Enter price"
+              className="input-form w-auto bg-secondary focus:bg-neutral-700"
             />
           </div>
         </form>
-        <button className="bg-orange-400" onClick={closeModal}>
-          Close
-        </button>
-      </div>
-    </div>
+      </Modal.Content>
+      <Modal.Actions></Modal.Actions>
+    </Modal>
   );
 };
 
-export default EditProductPop;
+export default AddProductPopup;
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -10,10 +10,14 @@ const ProductCard = () => {
       <div className="flex flex-row ml-5 w-[250px]">
         <div className="border-2 flex flex-col gap-1 py-3 px-4 mx-auto w-[800px] border-gray-900 ">
           <div className="flex justify-end">
+            <div className="flex justify-end">
+              <EditProductPop />
+            </div>
             {/* {auth.userData.has_store ? (
               <EditProductPop />
             ) : null} */}
           </div>
+
           <div className="gap-2 mt-8 flex justify-center ">
             <img
               className="object-cover w-max max-h-[200px] rounded-lg hover:transform  hover:scale-125 hover:-translate-y-5 transition duration-200 ease-in-out"
