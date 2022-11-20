148c9b11576411749e619f16f58fcd16d3995830
Tolga Uğur <29550889+Tolgaugur@users.noreply.github.com>
2022-11-20 16:54:07 +0300

    cart

--- src/screens/Cart.jsx
+++ src/screens/Cart.jsx
@@ -13,13 +13,22 @@ import { CartContext } from "../context/CartContext";
 const Cart = () => {
   const [isCartEmpty, setIsCartEmpty] = useState(false);
 
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [address, setAddress] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
   const GlobalState = useContext(CartContext);
   const dispatch = GlobalState.dispatch;
 
   const cart = useContext(CartContext);
   const auth = useAuth();
   const navigate = useNavigate();
+  const [number, setNumber] = useState(1);
 
+  const state = GlobalState.state;
   useEffect(() => {
     if (cart.state.length === 0) {
       setIsCartEmpty(true);
@@ -128,23 +137,94 @@ const Cart = () => {
         ) : (
           <>
             {/* {state.map((product) => { */}
-            <div className="flex justify-center bg-gray-900 pb-36 flex-wrap gap-[20px] mt-5">
-              <div className="mt-10 w-[70vw]  h-full bg-gray-900">
-                <div className="flex justify-center">
-                  <div className="flex flex-col justify-center mt-10 mb-10 h-full gap-10">
-                    <CartProducts />
-                    <div className="flex justify-start gap-10 w-full">
-                      <button
-                        className="btn-primary w-full"
-                        onClick={handleBuy}
-                      >
-                        Buy
-                      </button>
-                      <button className="btn-primary w-64" onClick={RemoveAll}>
-                        Remove All
-                      </button>
+            <div className="flex">
+              <div className="flex justify-center bg-gray-900 pb-36 flex-wrap gap-[20px] mt-5">
+                <div className="mt-10 w-[70vw]  h-full bg-gray-900">
+                  <div className="flex justify-center">
+                    <div className="flex flex-col justify-center mt-10 mb-10 h-full gap-10">
+                      <CartProducts />
+                      <div className="flex justify-start gap-10 w-full">
+                        <button
+                          className="btn-primary w-full"
+                          onClick={handleBuy}
+                        >
+                          Buy
+                        </button>
+                        <button
+                          className="btn-primary w-64"
+                          onClick={RemoveAll}
+                        >
+                          Remove All
+                        </button>
+                      </div>
                     </div>
                   </div>
+                </div>
+              </div>
+              <div className=" pl-6 border-0 mr-2 border-l-2 ml-7 w-[500px] flex-col  h-auto mt-5 max-h-xs bg-gray-900  border-white">
+                {/* ITEMS LIST */}
+
+                <div>
+                  {/* <div>{product.name}</div> */}
+                  <h1 className="text-gray-300 	text-decoration-line: underline">
+                    Items in Cart
+                  </h1>
+                  <div className="flex justify-between">
+                    <h2></h2>
+                  </div>
+                </div>
+
+                <div className="w-1/2">
+                  <label
+                    htmlFor="address"
+                    className="block mb-2 text-start w-auto text-base font-medium text-gray-300"
+                  >
+                    Address
+                  </label>
+                  <textarea
+                    type="address"
+                    name="floating_address"
+                    id="floating_address"
+                    placeholder={address}
+                    className="input-form p-4 h-[115px] bg-secondary focus:bg-neutral-700 block  w-full text-base rounded-lg border  text-gray-400 focus:outline-none border-primary placeholder-gray-400"
+                    // onChange={(e) => setAddress(e.target.value)}
+                  />
+                </div>
+                <div className=" w-1/2">
+                  <label
+                    htmlFor="telephone"
+                    className="block mb-2 text-start w-auto text-base font-medium text-gray-300"
+                  >
+                    Telephone
+                  </label>
+                  <input
+                    type="telephone"
+                    name="floating_telephone"
+                    id="floating_telephone"
+                    placeholder={phoneNumber}
+                    className="input-form bg-secondary focus:bg-neutral-700"
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                  />
+                </div>
+                <div className="mb-5 w-1/2">
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-start w-auto text-base font-medium text-gray-300"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="floating_email"
+                    id="floating_email"
+                    placeholder={email}
+                    className="input-form focus:bg-neutral-700 block  w-full text-base rounded-lg border text-gray-400 focus:outline-none bg-secondary border-primary placeholder-gray-400"
+                  />
+                </div>
+                <div className="flex justify-start pl-2">
+                  <button type="submit" className="btn-secondary">
+                    Confirm Order
+                  </button>
                 </div>
               </div>
             </div>
