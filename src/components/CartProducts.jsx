e8258eaa450d793dc909576cf8a3174193d175c9
Tolga Uğur <29550889+Tolgaugur@users.noreply.github.com>
2022-11-20 14:28:18 +0300

    bunu yazanı

--- src/components/CartProducts.jsx
+++ src/components/CartProducts.jsx
@@ -16,52 +16,61 @@ const CartProducts = () => {
     <div className="">
       {state.map((product) => {
         return (
-          <div className="w-full  max-w-xs h-[400px] mt-5 max-h-xs bg-gray-900 border border-white">
-            <img
-              className="p-2 object-cover h-48 w-full border border-gray-900 rounded-2xl"
-              src={product.supplier_prod_image}
-              alt="product image"
-            />
-            <div className="px-5 pb-5">
-              <h5 className="text-xl font-semibold tracking-tight text-white">
-                {product.name}
-              </h5>
-              <div className="flex justify-between items-center">
-                <span className="text-3xl font-bold text-white">
-                  ${product.price * product.quantity}
-                </span>
-                <div
-                  className="text-white focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center
-           bg-blue-600 hover:bg-blue-700 focus:ring-blue-800 border border-blue-200"
-                >
-                  <button
-                    className="text-3xl -mb-4 pl-2 pr-2 "
-                    onClick={() =>
-                      dispatch({ type: "INCREASE", payload: product })
-                    }
-                  >
-                    +
-                  </button>
-                  <h1>{product.quantity}</h1>
-                  <button
-                    className="text-3xl -mb-4 pl-2 pr-2"
-                    onClick={() => {
-                      if (product.quantity > 1)
-                        dispatch({ type: "DECREASE", payload: product });
-                      else dispatch({ type: "REMOVE", payload: product });
-                    }}
-                  >
-                    -
-                  </button>
-                  <div>
-                    <button
-                      className="text-3xl -mb-4 pl-2 pr-2"
-                      onClick={() =>
-                        dispatch({ type: "REMOVE", payload: product })
-                      }
+          <div className="w-[700px]  h-auto mt-5 max-h-xs bg-gray-900 border border-white">
+            <div className="flex justify-between">
+              <div className="flex flex-row">
+                <img
+                  className="p-2 flex justify-start object-cover h-48 w-full border border-gray-900 rounded-2xl"
+                  src={product.supplier_prod_image}
+                  alt="product image"
+                />
+                <h5 className="flex items-center justify-center mt-0 text-xl font-semibold  tracking-tight  text-white">
+                  {product.name}
+                </h5>
+              </div>
+
+              <div className="">
+                <div className="flex ">
+                  <div className="flex items-center  ">
+                    <span className="text-3xl font-bold text-white">
+                      ${product.price * product.quantity}
+                    </span>
+                  </div>
+                  <div className="flex mt-2 ">
+                    <div
+                      className=" flex flex-col items-center ml-5 text-white focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center
+                      bg-blue-600 hover:bg-blue-700 focus:ring-blue-800 border border-blue-200"
                     >
-                      X
-                    </button>
+                      <button
+                        className="text-3xl -mb-4 px-2 "
+                        onClick={() =>
+                          dispatch({ type: "INCREASE", payload: product })
+                        }
+                      >
+                        +
+                      </button>
+                      <h1>{product.quantity}</h1>
+                      <button
+                        className="text-3xl -mb-4 pl-2 pr-2"
+                        onClick={() => {
+                          if (product.quantity > 1)
+                            dispatch({ type: "DECREASE", payload: product });
+                          else dispatch({ type: "REMOVE", payload: product });
+                        }}
+                      >
+                        -
+                      </button>
+                      <div>
+                        <button
+                          className="text-3xl -mb-4 pt-3 pl-2 pr-2"
+                          onClick={() =>
+                            dispatch({ type: "REMOVE", payload: product })
+                          }
+                        >
+                          X
+                        </button>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
