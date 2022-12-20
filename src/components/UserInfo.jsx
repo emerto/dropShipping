56322adee3dfe9652f7c945a668b12f5b6709f3b
Tolga Uğur <29550889+Tolgaugur@users.noreply.github.com>
2022-12-20 11:59:50 +0300

    a

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -144,7 +144,7 @@ const Profile = () => {
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8 bg-secondary border-primary">
               <h1 className="font-bold leading-tight tracking-tight text-2xl text-white">
                 USER
-                <span className="text-black bg-primary ml-1 p-2 py-[2px] rounded-3xl :">
+                <span className="text-black bg-primary ml-1 p-2 py-[2px] rounded-lg :">
                   PROFILE
                 </span>
               </h1>
