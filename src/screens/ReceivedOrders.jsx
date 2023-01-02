8c30a23d33fe56adb4769412ec584efc3b3e1eb5
zeNn-G <treroin@gmail.com>
2023-01-02 19:27:09 +0300

    Reject - Refund

--- src/screens/ReceivedOrders.jsx
+++ src/screens/ReceivedOrders.jsx
@@ -6,9 +6,12 @@ import Navbar from "../components/Navbar";
 import { CheckBadgeIcon, XCircleIcon } from "@heroicons/react/24/solid";
 import OrderCard from "../components/OrderCard";
 
+import Spinner from "../utils/Spinner";
+
 const ReceivedOrders = () => {
   const [orders, setOrders] = useState([]);
   const [stock, setStock] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const auth = useAuth();
 
   const getOrders = async () => {
@@ -37,6 +40,7 @@ const ReceivedOrders = () => {
 
     if (data) {
       setOrders(data);
+      setIsLoading(false);
     }
   };
 
@@ -122,7 +126,43 @@ const ReceivedOrders = () => {
   };
 
   const rejectOrder = async (id) => {
-    const { data, error } = await supabase.from("orders");
+    const { data, error } = await supabase
+      .from("orders")
+      .select()
+      .eq("id", id)
+      .single();
+
+    if (error) {
+      console.log(error);
+    }
+
+    if (data) {
+      const { data: balance, error } = await supabase
+        .from("profiles")
+        .select("balance")
+        .eq("id", data.customer_id)
+        .single();
+
+      if (error) {
+        console.log(error);
+      }
+
+      if (balance) {
+        let newBalance = balance.balance + data.total;
+        const { data: update, error } = await supabase
+          .from("profiles")
+          .update({ balance: newBalance })
+          .eq("id", data.customer_id);
+
+        if (error) {
+          console.log(error);
+        }
+
+        if (update) {
+          updateOrderStatus(id, "rejected");
+        }
+      }
+    }
   };
 
   const updateOrderStatus = async (id, status) => {
@@ -155,77 +195,87 @@ const ReceivedOrders = () => {
             </h1>
           </header>
           <div className="w-full h-1 bg-primary rounded-xl mt-3" />
-          <section className="flex flex-col justify-center items-center ">
-            {orders.map((order) => {
-              return (
-                <div
-                  className="flex flex-col mt-5 bg-slate-700/20 p-2 w-[800px] rounded-xl"
-                  key={order.id}
-                >
-                  <div className="flex flex-col">
-                    <div className="flex flex-row justify-between">
-                      <h1 className="text-3xl text-white tracking-wider">
-                        Order ID:{" "}
-                        <span className="text-primary">{order.id}</span>
-                      </h1>
-                    </div>
-                    <div className="flex flex-row justify-between">
-                      <h1 className="text-3xl text-white tracking-wider">
-                        Order Date:{" "}
-                        <span className="text-primary">{order.order_date}</span>
-                      </h1>
-                    </div>
-                  </div>
-                  <div className="flex flex-col mt-5" key={order.id}>
-                    {order.carts.map((cart) => {
-                      return <OrderCard cart={cart} />;
-                    })}
-                  </div>
-                  <div className="w-full h-1 bg-primary rounded-xl mt-3" />
-                  <div className="flex flex-col items-end p-3">
-                    <div className="flex flex-col items-center">
-                      <div className="flex justify-end w-full  text-3xl text-white">
-                        <p>
-                          Total:
-                          <span className="text-primary"> ${order.total}</span>
-                        </p>
+          {!isLoading ? (
+            <section className="flex flex-col justify-center items-center ">
+              {orders.map((order) => {
+                return (
+                  <div
+                    className="flex flex-col mt-5 bg-slate-700/20 p-2 w-[800px] rounded-xl"
+                    key={order.id}
+                  >
+                    <div className="flex flex-col">
+                      <div className="flex flex-row justify-between">
+                        <h1 className="text-3xl text-white tracking-wider">
+                          Order ID:{" "}
+                          <span className="text-primary">{order.id}</span>
+                        </h1>
                       </div>
-                      {order.status === "pending" ? (
-                        <div className="flex mt-3 gap-3">
-                          <button
-                            className="btn-primary"
-                            onClick={() => {
-                              acceptOrder(order.id);
-                            }}
-                          >
-                            Accept
-                          </button>
-                          <button
-                            className="btn-primary"
-                            onClick={() => {
-                              updateOrderStatus(order.id, "rejected");
-                            }}
-                          >
-                            Reject
-                          </button>
+                      <div className="flex flex-row justify-between">
+                        <h1 className="text-3xl text-white tracking-wider">
+                          Order Date:{" "}
+                          <span className="text-primary">
+                            {order.order_date}
+                          </span>
+                        </h1>
+                      </div>
+                    </div>
+                    <div className="flex flex-col mt-5" key={order.id}>
+                      {order.carts.map((cart) => {
+                        return <OrderCard cart={cart} />;
+                      })}
+                    </div>
+                    <div className="w-full h-1 bg-primary rounded-xl mt-3" />
+                    <div className="flex flex-col items-end p-3">
+                      <div className="flex flex-col items-center">
+                        <div className="flex justify-end w-full  text-3xl text-white">
+                          <p>
+                            Total:
+                            <span className="text-primary">
+                              {" "}
+                              ${order.total}
+                            </span>
+                          </p>
                         </div>
-                      ) : order.status === "accepted" ? (
-                        <div className="flex flex-row items-center text-3xl text-white ">
-                          <CheckBadgeIcon className="h-[24px] w-[24px] text-green-500" />
-                          <p>Accepted</p>
-                        </div>
-                      ) : (
-                        <div className="flex items-center text-3xl text-white ">
-                          <XCircleIcon className="h-[24px] w-[24px] text-red-500" />
-                          <p>Rejected, money will be refunded</p>
-                        </div>
-                      )}
+                        {order.status === "pending" ? (
+                          <div className="flex mt-3 gap-3">
+                            <button
+                              className="btn-primary"
+                              onClick={() => {
+                                acceptOrder(order.id);
+                              }}
+                            >
+                              Accept
+                            </button>
+                            <button
+                              className="btn-primary"
+                              onClick={() => {
+                                // updateOrderStatus(order.id, "rejected");
+                                rejectOrder(order.id);
+                              }}
+                            >
+                              Reject
+                            </button>
+                          </div>
+                        ) : order.status === "accepted" ? (
+                          <div className="flex flex-row items-center text-3xl text-white ">
+                            <CheckBadgeIcon className="h-[24px] w-[24px] text-green-500" />
+                            <p>Accepted</p>
+                          </div>
+                        ) : (
+                          <div className="flex items-center text-3xl text-white ">
+                            <XCircleIcon className="h-[24px] w-[24px] text-red-500" />
+                            <p>Rejected, money will be refunded</p>
+                          </div>
+                        )}
+                      </div>
                     </div>
                   </div>
-                </div>
-              );
-            })}
-          </section>
+                );
+              })}
+            </section>
+          ) : (
+            <Spinner />
+          )}
         </section>
       </div>
     </div>
