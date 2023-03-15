f47938996ec79986b87b188ce49d1ff46db555c8
zeNn-G <treroin@gmail.com>
2023-03-15 18:06:20 +0300

    O gitti ben kaldım

--- src/config/supaBaseClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://tcvbahslxgfxsxqidkyy.supabase.co";
-const supabaseKey = import.meta.env.VITE_APP_SUPABASE_KEY;
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
