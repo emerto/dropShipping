96198934526e647f34594c636ff85ceabaef1896
zeNn-G <treroin@gmail.com>
2023-03-17 01:38:19 +0300

    Profile

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,37 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { supabase } from "../utils/supabaseClient";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Navbar from "../components/Navbar";
-import { useAuthStore } from "../stores/useAuthStore";
 
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
+import { supabaseClient } from "../utils/supabaseBrowserClient";
+
 import { useUser } from "@supabase/auth-helpers-react";
+import { useAuthStore } from "../stores/useAuthStore";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const user = useUser();
 
-  if (user) {
-    console.log(user);
+  async function fetchUser() {
+    const { data: userData, error } = await supabaseClient
+      .from("profiles")
+      .select("*")
+      .eq("id", user.id)
+      .single();
+
+    if (userData) {
+      useAuthStore.setState({
+        userStore: userData,
+      });
+    }
   }
+
+  if (user) {
+    fetchUser();
+  }
+
   return (
     <div>
       <Navbar children={children} />
