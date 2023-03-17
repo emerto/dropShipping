9041c0eec6367c35631a7046c7d93ca7f49b184b
zeNn-G <treroin@gmail.com>
2023-03-17 14:37:20 +0300

    Revert "Nav"
    This reverts commit 8f81cfd55b4a8ac7ddb13f284bde913f95b5cc22.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Inter } from "next/font/google";
+import Navbar from "../components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <h1>Dropshoop</h1>
     </main>
   );
