69558ca158aac7af9097d5130a09e25b29c776f6
zeNn-G <treroin@gmail.com>
2023-03-20 16:41:11 +0300

    Adding Product

---
+++ components/ProductDropdown.tsx
@@ -0,0 +1,106 @@
+import Select from "react-select";
+import Image from "next/image";
+import type { Database } from "../types/supabase";
+
+type supplierProducts =
+  Database["public"]["Tables"]["supplier_products"]["Row"];
+
+import React from "react";
+
+type Props = {
+  setSelectedProduct: (e: any) => void;
+  suppProds: supplierProducts[];
+};
+
+type Option = {
+  innerRef: any;
+  innerProps: any;
+  data: supplierProducts;
+};
+
+const Dropdown = ({ setSelectedProduct, suppProds }: Props) => {
+  const mapped = suppProds.map((prod) => ({
+    value: prod.id,
+    label: prod.name,
+    data: prod,
+  }));
+
+  const CustomOption = ({ innerRef, innerProps, data }) => {
+    const product = data.data as supplierProducts;
+    return (
+      <div
+        {...innerProps}
+        ref={innerRef}
+        className="flex items-center lg:justify-between bg-base-100 hover:cursor-pointer hover:bg-base-200"
+      >
+        <div className="flex items-center">
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={100}
+            height={100}
+            className="w-20 h-20"
+          />
+          <div className="ml-2 flex flex-col lg:flex-row">
+            <span className="text-lg">{product.name}</span>
+            <div className="flex lg:hidden gap-2 text-sm">
+              <p>
+                Price: ${product.price} <span className="text-primary">|</span>{" "}
+                Stock: {product.stock}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="hidden lg:flex gap-2">
+          <p>
+            Price: ${data.price} <span className="text-primary">|</span> Stock:{" "}
+            {data.stock}
+          </p>
+        </div>
+      </div>
+    );
+  };
+
+  const customStyles = {
+    control: (provided: any) => ({
+      ...provided,
+      backgroundColor: "transparent",
+      border: "none",
+      borderRadius: "0.5rem",
+    }),
+    menu: (provided: any) => ({
+      ...provided,
+      backgroundColor: "transparent",
+      zIndex: 9999,
+    }),
+    option: (provided: any) => ({
+      ...provided,
+      backgroundColor: "transparent",
+      border: "none",
+      borderRadius: "0.5rem",
+    }),
+  };
+
+  return (
+    <Select
+      id="addProduct"
+      instanceId="addProduct"
+      options={mapped}
+      className="border-2 border-primary rounded-md"
+      isSearchable
+      placeholder="Select a product"
+      styles={customStyles}
+      filterOption={(option, rawInput) => {
+        const input = rawInput.toLowerCase();
+        const name = option.label.toLowerCase();
+        return name.includes(input);
+      }}
+      onChange={(e) => setSelectedProduct(e)}
+      menuPortalTarget={typeof document !== "undefined" ? document.body : null}
+      components={{ Option: CustomOption }}
+      menuPlacement="bottom"
+    />
+  );
+};
+
+export default Dropdown;
--- pages/manageStore/index.tsx
+++ pages/manageStore/index.tsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import type { Database } from "../../types/supabase";
+import { Icon } from "@iconify/react";
+import { useForm } from "react-hook-form";
+import { supabase } from "../../utils/supabaseClient";
+import toast from "react-hot-toast";
+
+import ProductDropdown from "../../components/ProductDropdown";
 
 type store = Database["public"]["Tables"]["stores"]["Row"];
 type products = Database["public"]["Tables"]["products"]["Row"];
+type supplierProducts =
+  Database["public"]["Tables"]["supplier_products"]["Row"];
 
 type Props = {
   store: store;
   products: products[];
+  supplierProducts: supplierProducts[];
+};
+
+type Inputs = {
+  productName: string;
+  price: number;
+  supplierProductImage: string;
+  storeId: string;
+};
+
+type selectedProduct = {
+  value: number;
+  label: string;
+  data: supplierProducts;
 };
 
 export const getServerSideProps = async (ctx) => {
@@ -57,20 +80,159 @@ export const getServerSideProps = async (ctx) => {
     console.log(productsError);
   }
 
+  const { data: supplierProducts, error: supplierProductsError } =
+    await supabase.from("supplier_products").select("*");
+
+  if (supplierProductsError) {
+    console.log(supplierProductsError);
+  }
+
   return {
     props: {
       store,
       products,
+      supplierProducts,
     },
   };
 };
 
-const ManageStore = ({ store, products }: Props) => {
+const ManageStore = ({ store, products, supplierProducts }: Props) => {
+  const [selectedProduct, setSelectedProduct] = useState<selectedProduct>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<Inputs>();
+
+  const onSubmit = async (data: Inputs) => {
+    if (!selectedProduct?.data) {
+      toast.error("Please select a product!");
+      return;
+    }
+
+    const suppId = selectedProduct?.data?.id;
+    const suppImage = selectedProduct?.data?.image;
+
+    const { error } = await supabase.from("products").insert({
+      name: data.productName,
+      price: data.price,
+      store_id: store.id,
+      supplier_product_id: suppId,
+      supplier_prod_image: suppImage,
+    });
+
+    if (error) {
+      toast.error(error.message);
+      return;
+    }
+
+    toast.success("Product added successfully!");
+
+    reset();
+  };
+
   return (
-    <>
-      <pre>{JSON.stringify(store, null, 2)}</pre>
-      <pre>{JSON.stringify(products, null, 2)}</pre>
-    </>
+    <section className="flex flex-col w-full">
+      <div className="flex justify-between items-center">
+        <h1 className="text-xl lg:text-3xl font-semibold lg:font-bold text-base-content">
+          {store.store_name}
+        </h1>
+        {/* {Modal} */}
+        <label htmlFor="my-modal" className="btn btn-primary">
+          Add Product
+        </label>
+        <input type="checkbox" id="my-modal" className="modal-toggle" />
+        <div className="modal">
+          <div className="modal-box max-w-5xl">
+            <div className="flex justify-between items-center">
+              <h3 className="font-bold text-lg">Add Product</h3>
+              <label htmlFor="my-modal" className="btn">
+                <Icon icon="ic:baseline-close" className="w-[16px] h-[16px]" />
+              </label>
+            </div>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text">Product Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Cool Product"
+                  {...register("productName", {
+                    required: true,
+                    minLength: 2,
+                    maxLength: 40,
+                  })}
+                  className="input input-bordered w-full "
+                />
+                {errors.productName?.type === "required" && (
+                  <span className="mt-1 text-sm text-red-600">
+                    This field is required
+                  </span>
+                )}
+                {errors.productName?.type === "minLength" && (
+                  <span className="mt-1 text-sm text-red-600">
+                    This field must be at least 2 characters
+                  </span>
+                )}
+                {errors.productName?.type === "maxLength" && (
+                  <span className="mt-1 text-sm text-red-600">
+                    This field must be at most 40 characters
+                  </span>
+                )}
+              </div>
+              <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text">Product Price</span>
+                </label>
+                <input
+                  type="number"
+                  placeholder="420.69$"
+                  {...register("price", {
+                    required: true,
+                    min: 0.01,
+                  })}
+                  className="input input-bordered w-full "
+                />
+                {errors.price?.type === "required" && (
+                  <span className="mt-1 text-sm text-red-600">
+                    This field is required
+                  </span>
+                )}
+                {errors.price?.type === "min" && (
+                  <span className="mt-1 text-sm text-red-600">
+                    Price must be greater than 0
+                  </span>
+                )}
+              </div>
+              <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text">Select Product</span>
+                </label>
+                <ProductDropdown
+                  setSelectedProduct={setSelectedProduct}
+                  suppProds={supplierProducts}
+                />
+              </div>
+              <div className="modal-action">
+                <button className="btn btn-primary" type="submit">
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className="divider h-fit bg-primary" />
+      {/* {Products} */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {products?.map((product) => (
+          <h1 key={product.id}>{product.name}</h1>
+        ))}
+      </div>
+    </section>
   );
 };
 
