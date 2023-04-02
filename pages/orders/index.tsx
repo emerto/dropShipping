e03a598e9cdbd565083c237d821fd64fc0069de9
zeNn-G <treroin@gmail.com>
2023-04-02 19:53:47 +0300

    Orders

--- pages/orders/index.tsx
+++ pages/orders/index.tsx
@@ -1,7 +1,188 @@
-import { NextPage } from "next";
+import {
+  GetServerSidePropsContext,
+  NextApiRequest,
+  NextApiResponse,
+  PreviewData,
+} from "next";
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import type { Database } from "../../types/supabase";
+import Image from "next/image";
+import { ParsedUrlQuery } from "querystring";
+import { Icon } from "@iconify/react";
 
-const Orders: NextPage = () => {
-  return <div>Orders</div>;
+type Order = {
+  id: number;
+  status: string;
+  order_date: string;
+  total: number;
+  carts: Array<{
+    order_id: number;
+    product_id: number;
+    amount: number;
+    products: {
+      id: number;
+      supplier_product_id: number;
+      name: string;
+      price: number;
+      supplier_prod_image: string;
+      store_id: number;
+    };
+  }>;
+};
+
+type Props = {
+  orders: Order[];
+};
+
+export const getServerSideProps = async (
+  ctx:
+    | GetServerSidePropsContext<ParsedUrlQuery, PreviewData>
+    | { req: NextApiRequest; res: NextApiResponse<any> }
+) => {
+  const supabase = createServerSupabaseClient(ctx);
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  const { data: orders, error } = await supabase
+    .from("orders")
+    .select(
+      `
+      id,
+      status,
+      order_date,
+      total,
+      carts(
+        *,
+        products(
+          *
+        )
+        )
+      `
+    )
+    .eq("customer_id", session?.user?.id)
+    .order("order_date", { ascending: false });
+
+  if (error) {
+    console.log(error);
+  }
+
+  return {
+    props: {
+      orders,
+    },
+  };
+};
+
+const Orders = ({ orders }: Props) => {
+  return (
+    <main>
+      <h1 className="text-xl lg:text-3xl font-semibold lg:font-bold text-base-content">
+        Orders
+      </h1>
+      <div className="divider h-fit bg-primary" />
+      <div className="flex justify-center items-center">
+        <section className="flex flex-col lg:w-1/2 w-full mt-5 gap-5">
+          {orders.map((order) => {
+            return (
+              <div
+                key={order.id}
+                className="flex flex-col p-5 bg-base-300 rounded-xl"
+              >
+                <div className="flex flex-col">
+                  <h1 className="text-xl lg:text-3xl">
+                    Order Id: <span className="text-primary">{order.id}</span>
+                  </h1>
+                  <h1 className="text-xl lg:text-3xl">
+                    Order Date:{" "}
+                    <span className="text-primary">
+                      {new Date(order.order_date).toLocaleDateString()}
+                    </span>
+                  </h1>
+                </div>
+                <div className="flex flex-col justify-between mt-2 gap-10">
+                  {order.carts.map((cart) => {
+                    return (
+                      <div
+                        className="flex lg:flex-row flex-col"
+                        key={cart.product_id}
+                      >
+                        <Image
+                          src={cart.products.supplier_prod_image}
+                          width={200}
+                          height={200}
+                          alt={cart.products.name}
+                          className="rounded-xl w-full lg:h-52 lg:w-52 object-cover"
+                        />
+                        <div className="flex lg:flex-row flex-col justify-between lg:items-center w-full">
+                          <span className="text-xl lg:text-2xl ml-2">
+                            {cart.products.name}
+                          </span>
+                          <div className="flex flex-col lg:items-center ml-2">
+                            <span className="text-lg lg:text-2xl">
+                              ${cart.products.price}
+                            </span>
+                            <span>
+                              <span className="text-base">
+                                {cart.products.price}
+                                <span className="text-primary">
+                                  x{cart.amount}
+                                </span>
+                              </span>
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+                <div className="divider h-fit bg-primary" />
+                <div className="flex flex-col items-end">
+                  <span className="text-2xl">
+                    Total: <span className="text-primary">${order.total}</span>
+                  </span>
+                  {order.status === "pending" && (
+                    <div className="flex items-center">
+                      <Icon
+                        icon="material-symbols:clock-loader-10"
+                        className="text-primary w-5 h-5"
+                      />
+                      <span className="text-xl ml-1">Pending</span>
+                    </div>
+                  )}
+                  {order.status === "accepted" && (
+                    <div className="flex items-center">
+                      <Icon
+                        icon="carbon:checkmark-filled"
+                        className="text-success w-5 h-5"
+                      />
+                      <span className="text-xl ml-1">Delivered</span>
+                    </div>
+                  )}
+                  {order.status === "rejected" && (
+                    <>
+                      <div className="flex items-center">
+                        <Icon
+                          icon="carbon:close-filled"
+                          className="text-error w-5 h-5"
+                        />
+                        <span className="text-xl ml-1">
+                          Rejected By Dropshipper
+                        </span>
+                      </div>
+                      <p className="text-error">
+                        Money will be refunded to your account.
+                      </p>
+                    </>
+                  )}
+                </div>
+              </div>
+            );
+          })}
+        </section>
+      </div>
+    </main>
+  );
 };
 
 export default Orders;
