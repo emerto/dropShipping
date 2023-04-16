8b771a8973f24dc043a2e293deea362ef4945695
zeNn-G <treroin@gmail.com>
2023-04-16 16:17:19 +0300

    Disable lint+type

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   swcMinify: true,
   images: {
     domains: ["tcvbahslxgfxsxqidkyy.supabase.co", "via.placeholder.com"],
