bbd7959e6e1ef8e38e837d3b07398f3f00a3f04b
Erkan Ozcanoglu <ozcanogerkan12@gmail.com>
2023-04-16 14:54:03 +0300

    404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,4 @@
-import NotFount from "../public/404-computer.svg";
+import Link from "next/link";
 
 export default function Custom404() {
   return (
@@ -15,11 +15,11 @@ export default function Custom404() {
             Sorry, we can't find that page. You'll find lots to explore on the
             home page.
           </p>
-          <a
-            href="#"
-            className="inline-flex text-primary bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">
+          <Link
+            href="/"
+            className="btn inline-flex text-primary bg-primary-600 my-4">
             Back to Homepage
-          </a>
+          </Link>
         </div>
       </div>
     </section>
