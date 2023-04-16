d635dad74adb393b23a8f3d44a4eaebc721c088f
Erkan Ozcanoglu <ozcanogerkan12@gmail.com>
2023-04-16 14:54:53 +0300

    404

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Custom404() {
   return (
-    <section className="h-full min-h-[531px] bg-white dark:bg-gray-900">
+    <section className="h-full min-h-[531px]">
       <div className="w-full h-full flex justify-center items-center py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div className="mx-auto max-w-screen-sm text-center">
           <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary">
